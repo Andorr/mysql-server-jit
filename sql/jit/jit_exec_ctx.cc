df14b0b763f10d24282b312a38924aabe2a2f583
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2022-01-21 13:57:19 +0100

    llvm: updated cmake config

--- sql/jit/jit_exec_ctx.cc
+++ sql/jit/jit_exec_ctx.cc
@@ -1,5 +1,10 @@
 #include "memory"
 
+#include "llvm/ADT/StringRef.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Value.h"
+
 #include "jit_exec_ctx.h"
 
 using namespace jit;
