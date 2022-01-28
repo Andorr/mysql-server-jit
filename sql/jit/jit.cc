eb8115eb75436ad4c2b9f1bb6a926a6905758f37
Sveinung Øverland <sveinungg.overlandd@gmail.com>
2022-01-28 17:06:21 +0100

    Implemented creating Item_compiled from Item

--- sql/jit/jit.cc
+++ sql/jit/jit.cc
@@ -12,7 +12,9 @@ std::unique_ptr<JITExecutionContext> new_jit_exec_ctx() {
   return JITExecutionContext::new_exec_context();
 }
 Item_compiled *jit::create_item_compiled_from_item(
-    JITExecutionContext *jit_exec_ctx, Item *where_cond) {
-  // TODO: Implement this
-  return nullptr;
+    JITExecutionContext *jit_exec_ctx, Item *item) {
+  Item_compiled compiled = Item_compiled(jit_exec_ctx, item);
+  compiled.codegen_item();
+  compiled.jit_compile(jit_exec_ctx);
+  return &compiled;
 }
