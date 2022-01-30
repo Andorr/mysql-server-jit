7980718fbfdcb53950d28bf772616c767303b91f
Sveinung Øverland <sveinungg.overlandd@gmail.com>
2022-01-30 16:57:27 +0100

    Fixed local variable warning

--- sql/jit/jit.cc
+++ sql/jit/jit.cc
@@ -13,8 +13,8 @@ std::unique_ptr<JITExecutionContext> new_jit_exec_ctx() {
 }
 Item_compiled *jit::create_item_compiled_from_item(
     JITExecutionContext *jit_exec_ctx, Item *item) {
-  Item_compiled compiled = Item_compiled(jit_exec_ctx, item);
-  compiled.codegen_item();
-  compiled.jit_compile(jit_exec_ctx);
-  return &compiled;
+  Item_compiled *compiled = new Item_compiled(jit_exec_ctx, item);
+  compiled->codegen_item();
+  compiled->jit_compile(jit_exec_ctx);
+  return compiled;
 }
